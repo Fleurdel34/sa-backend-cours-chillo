79c26b59b95cf98690d1ca80f9bb050ab4df42d8
Fleurdel34 <betdevelweb0953@gmail.com>
2025-05-24 20:35:28 +0200

    created client service

--- src/main/java/tech/chilo/sa/repository/ClientRepository.java
+++ src/main/java/tech/chilo/sa/repository/ClientRepository.java
@@ -1,9 +1,10 @@
 package tech.chilo.sa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 import tech.chilo.sa.entities.Client;
 
-
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 }
--- src/main/java/tech/chilo/sa/service/ClientService.java
+++ src/main/java/tech/chilo/sa/service/ClientService.java
@@ -2,11 +2,18 @@ package tech.chilo.sa.service;
 
 import org.springframework.stereotype.Service;
 import tech.chilo.sa.entities.Client;
+import tech.chilo.sa.repository.ClientRepository;
 
 @Service
 public class ClientService {
 
-    public void creer(Client client){
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
 
+    private ClientRepository clientRepository;
+
+    public void creer(Client client){
+        this.clientRepository.save(client);
     }
 }
